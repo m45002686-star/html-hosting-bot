c29a0e132d06b2aead71655026858b4ff33d2af4
m45002686-star <m45002686@gmail.com>
2026-01-07 09:53:09 +0300

    Delete project file: script.js

--- projects/8220915719/80214d2d/script.js
+++
@@ -1,5 +0,0 @@
-// JavaScript إضافي للمشروع: 66
-console.log('موقع 66 محمل بنجاح - ID: 80214d2d');
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('جاهز للاستخدام');
-});
